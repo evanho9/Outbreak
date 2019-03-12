8e10b01c93a61f81ac65a58ebe5ff91dc644d54e
Evan Ho <evanho9@live.com>
2019-03-11 23:50:59 -0700

    Smooth jumping

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float upForce = 200f;
+    public float upForce = 10f;
   
     private bool isDead = false;
     private Rigidbody2D rb2d;
@@ -15,20 +15,22 @@ public class Player : MonoBehaviour
     {
       rb2d = GetComponent<Rigidbody2D>();
       anim = GetComponent<Animator>();
-      rb2d.velocity = new Vector2(0, 0);
+      rb2d.velocity = new Vector2(GameControl.instance.scrollSpeed, 0);
       anim.SetTrigger("Run"); 
     }
 
     // Update is called once per frame
     void Update()
     {
+      rb2d.velocity = new Vector2(GameControl.instance.scrollSpeed, rb2d.velocity.y);
       if (!isDead) {  
         //Jump
         GameControl.instance.AddScore(0.01f);
         if (Input.GetMouseButtonDown(0)) {
           Debug.Log("jumped");
-          rb2d.velocity = new Vector2(0, 0); 
           rb2d.AddForce(new Vector2(0, upForce));
+          //rb2d.velocity = new Vector2(rb2d.velocity.x, upForce); 
+          //rb2d.AddForce(Vector2.up * upForce, ForceMode2D.Impulse);
           anim.SetTrigger("Jump");
         }
       }
